6a51a2d7826200687725db41ef78a1cb2cf8c56a
TheMrEvil <supersurplus99@gmail.com>
2025-08-08 20:40:26 -0500

    made it work

--- Library.js
+++ Library.js
@@ -1,6 +1,7 @@
 InterjectCardName = "Interject";
 Interjecttext = ''
 
+
 function createIfNoNotepadCard() {
     if (!storyCards.find(sc => sc.title === InterjectCardName)) {
         addStoryCard(InterjectCardName, 'None.', InterjectCardName)
@@ -8,12 +9,16 @@ function createIfNoNotepadCard() {
         // Set up the card properties
         const InterjectCard = storyCards.find(sc => sc.title === InterjectCardName)
         InterjectCard.description = '--- how to use--\nanything typed in interject will be given to the ai then cleared\n\n-- - github-- -\nhttps://github.com/TheMrEvil/Interject'
+        InterjectCard.entry = ''
+        return
     }
 }
 function ICC(text) {
     createIfNoNotepadCard()
+    InterjectCard = storyCards.find(sc => sc.title === InterjectCardName)
     if (InterjectCard.entry != '') {
         text += '<SYSTEM MESSAGE> Please keep in mind: ' + InterjectCard.entry + '</SYSTEM MESSAGE>'
+        console.log('InterjectCard entry:', InterjectCard.entry)
     }
     InterjectCard.entry = ''
     return { text }
